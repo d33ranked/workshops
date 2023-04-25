a8836f2b55e807e8c38a10ee404c6a75e1a14947
d33ranked <74716592+d33ranked@users.noreply.github.com>
2023-04-25 20:51:21 +0200

    final product

---
+++ nextjs-newsapp/pages/500.tsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+    return ( 
+        <div>
+            <h1>Error 🥴</h1>
+            <p>Looks like something went wrong, please refresh the page or contact the support team.</p>
+        </div>
+
+    );
+}
+ 
+export default ErrorPage;
--- nextjs-newsapp/pages/index.tsx
+++ nextjs-newsapp/pages/index.tsx
@@ -1,19 +1,20 @@
 import NewsArticlesGrid from '@components/components/NewsArticlesGrid';
-import NewsArticleEntry from '@components/components/NewsarticleEntry';
 import { NewsArticle, NewsResponse } from '@components/models/NewsArticles';
 import { GetServerSideProps } from 'next';
-import Head from 'next/head'
+import Head from 'next/head';
 import { Alert } from 'react-bootstrap';
 
 interface BreakingNewsPageProps {
   newsArticles: NewsArticle[],
 }
+
 export const getServerSideProps: GetServerSideProps<BreakingNewsPageProps> = async () => {
-  const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.NEWS_API_KEY)
+  const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=" + process.env.NEWS_API_KEY);
   const newsResponse: NewsResponse = await response.json();
   return {
     props: { newsArticles: newsResponse.articles }
   }
+  // let error go to 500 page
 }
 
 export default function BreakingNewsPage({newsArticles} : BreakingNewsPageProps) {
